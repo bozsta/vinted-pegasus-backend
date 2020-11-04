7c96ad78aaf94a1282ab9d7289aec5f6722aa34b
Luboz fabien <fluboz@MacBook-Pro-de-Luboz.local>
2020-11-04 21:54:59 +0100

    test to fix heroku

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const formidable = require('express-formidable')
 const mongoose = require('mongoose')
 const cors = require('cors')
+
 require('dotenv').config()
+
+const app = express()
+app.use(formidable())
+app.use(cors)
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-const app = express()
-app.use(formidable())
-app.use(cors)
 
 // Routes
 const userRoutes = require('./routes/user')
