52ff4ae91835bc66d523462f149623f6ac043bb6
Luboz fabien <fluboz@MacBook-Pro-de-Luboz.local>
2020-11-04 22:17:11 +0100

    fix issue heroku

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const express = require("express");
-const formidable = require("express-formidable");
-const mongoose = require("mongoose");
-const cloudinary = require("cloudinary").v2;
-const cors = require("cors");
+const express = require('express')
+const formidable = require('express-formidable')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-require("dotenv").config();
+require('dotenv').config()
 
-const app = express();
-app.use(formidable());
-app.use(cors());
+const app = express()
+app.use(formidable())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
 
-// Import des routes
-const userRoutes = require("./routes/user");
-app.use(userRoutes);
-const offerRoutes = require("./routes/offers");
-app.use(offerRoutes);
+// Routes
+const userRoutes = require('./routes/user')
+app.use("/user", userRoutes)
+const offerRoutes = require('./routes/offers')
+app.use("/offer", offerRoutes)
 
 app.all("*", (req, res) => {
-  res.status(404).json({ message: "Cette route n'existe pas" });
-});
+  res.status(404).json({ message: 'URL not found'})
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on port : ${process.env.PORT}`);
-});
+    console.log(`Server start on port ${process.env.PORT}`)
+})
